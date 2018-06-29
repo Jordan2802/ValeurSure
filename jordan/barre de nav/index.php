e91af56c64aae7eca7f6a6c5bb9f93308636379f
jordan2802 <sha08dow@hotmail.fr>
2018-06-29 16:30:54 +0200

    include

--- jordan/barre de nav/index.php
+++ jordan/barre de nav/index.php
@@ -12,52 +12,9 @@
   </head>
   <body>
 <header id="head">
-    <nav>
-      <div class="logop">
-      <i class="fab fa-wordpress"></i>
-      </div>
-    <ul id="menuSlide">
-      <li><a href="#">Accueil</a></li>
-  	<li><a href="#">A	propos de ValeurSure</a>
-  		<ul>
-  			<li><a href="#">Présentation</a></li>
-  			<li><a href="#">Engagements</a></li>
-  			<li><a href="#">Valeurs</a></li>
-  		</ul>
-  	</li>
-  	<li><a href="#">Nos prestations</a>
-  		<ul>
-  			<li><a href="#">Crédits professionnels</a></li>
-  			<li><a href="#">Crédit immobiliers</a></li>
-  			<li><a href="#">Assurances Emprunteurs</a></li>
-  			<li><a href="#">Conseils pour gérer les finances de votre entreprise</a></li>
-        <li><a href="#">Formation</a></li>
-  		</ul>
-  	</li>
-  	<li><a href="#">Simulateurs</a>
-  		<ul>
-  			<li><a href="#">Capacité d'emprunt</a></li>
-  			<li><a href="#">Capital</a></li>
-  			<li><a href="#">Mensualité</a></li>
-  		</ul>
-  	</li>
-  	<li><a href="#">Demande de financement</a></li>
-    <li><a href="#">Contact</a></li>
-  </ul>
-  </nav>
-  <div id="slider">
-    <i class="fab fa-wordpress first"></i>
-    <div class="slider">
-
-      <div class="slide"></div>
-      <div class="slide"></div>
-      <div class="slide"></div>
-      <div class="slide"></div>
-
-    </div>
-  </div>
-  <div class="info"></div>
+<?php include('nav.php'); ?>
   </header>
+  <div class="info"></div>
   <div class="tel" id="tel">
     <div class="fas fa-mobile-alt" onclick="slideTel()"></div>
     <div class="text cache" id="tel">00-00-00-00-00, adresse@hotmail.fr <br>
---
+++ jordan/barre de nav/nav.php
@@ -0,0 +1,44 @@
+<nav>
+  <div class="logop">
+  <i class="fab fa-wordpress"></i>
+  </div>
+<ul id="menuSlide">
+  <li><a href="#">Accueil</a></li>
+<li><a href="#">A	propos de ValeurSure</a>
+  <ul>
+    <li><a href="#">Présentation</a></li>
+    <li><a href="#">Engagements</a></li>
+    <li><a href="#">Valeurs</a></li>
+  </ul>
+</li>
+<li><a href="#">Nos prestations</a>
+  <ul>
+    <li><a href="#">Crédits professionnels</a></li>
+    <li><a href="#">Crédit immobiliers</a></li>
+    <li><a href="#">Assurances Emprunteurs</a></li>
+    <li><a href="#">Conseils pour gérer les finances de votre entreprise</a></li>
+    <li><a href="#">Formation</a></li>
+  </ul>
+</li>
+<li><a href="#">Simulateurs</a>
+  <ul>
+    <li><a href="#">Capacité d'emprunt</a></li>
+    <li><a href="#">Capital</a></li>
+    <li><a href="#">Mensualité</a></li>
+  </ul>
+</li>
+<li><a href="#">Demande de financement</a></li>
+<li><a href="#">Contact</a></li>
+</ul>
+</nav>
+<div id="slider">
+<i class="fab fa-wordpress first"></i>
+<div class="slider">
+
+  <div class="slide"></div>
+  <div class="slide"></div>
+  <div class="slide"></div>
+  <div class="slide"></div>
+
+</div>
+</div>

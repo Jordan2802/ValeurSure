8ebc0f0623e610f3d79c0ad00765d0efd32eceab
jordan2802 <sha08dow@hotmail.fr>
2018-07-03 16:48:32 +0200

    mise a jour nav

--- jordan/barredenav/index.php
+++ jordan/barredenav/index.php
@@ -12,39 +12,7 @@
   </head>
   <body>
 <header id="head">
-  <nav id="tailleNav">
-  <ul id="menuSlide">
-    <div class="logop" id="logoNav">
-    <i class="fab fa-wordpress"></i>
-    </div>
-    <li><a href="#">Accueil</a></li>
-  <li><a href="#">A	propos de ValeurSure</a>
-    <ul>
-      <li><a href="#">Présentation</a></li>
-      <li><a href="#">Engagements</a></li>
-      <li><a href="#">Valeurs</a></li>
-    </ul>
-  </li>
-  <li><a href="#">Nos prestations</a>
-    <ul>
-      <li><a href="#">Crédits professionnels</a></li>
-      <li><a href="#">Crédit immobiliers</a></li>
-      <li><a href="#">Assurances Emprunteurs</a></li>
-      <li><a href="#">Conseils pour gérer les finances de votre entreprise</a></li>
-      <li><a href="#">Formation</a></li>
-    </ul>
-  </li>
-  <li><a href="#">Simulateurs</a>
-    <ul>
-      <li><a href="#">Capacité d'emprunt</a></li>
-      <li><a href="#">Capital</a></li>
-      <li><a href="#">Mensualité</a></li>
-    </ul>
-  </li>
-  <li><a href="#">Demande de financement</a></li>
-  <li><a href="#">Contact</a></li>
-  </ul>
-  </nav>
+  <?php include('nav.php'); ?>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script type="text/javascript" src="js/app.js"></script>
 <div id="slider">

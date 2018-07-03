4f14495cd49e84c50af96a893e042d149babdad9
jordan2802 <sha08dow@hotmail.fr>
2018-07-03 14:51:27 +0200

    maj

--- PROD/index.php
+++ PROD/index.php
@@ -12,7 +12,39 @@
   </head>
   <body>
 <header id="head">
-<?php include('nav.php'); ?>
+  <nav>
+  <ul id="menuSlide">
+    <div class="logop">
+    <i class="fab fa-wordpress"></i>
+    </div>
+    <li><a href="#">Accueil</a></li>
+  <li><a href="#">A	propos de ValeurSure</a>
+    <ul>
+      <li><a href="#">Présentation</a></li>
+      <li><a href="#">Engagements</a></li>
+      <li><a href="#">Valeurs</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Nos prestations</a>
+    <ul>
+      <li><a href="#">Crédits professionnels</a></li>
+      <li><a href="#">Crédit immobiliers</a></li>
+      <li><a href="#">Assurances Emprunteurs</a></li>
+      <li><a href="#">Conseils pour gérer les finances de votre entreprise</a></li>
+      <li><a href="#">Formation</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Simulateurs</a>
+    <ul>
+      <li><a href="#">Capacité d'emprunt</a></li>
+      <li><a href="#">Capital</a></li>
+      <li><a href="#">Mensualité</a></li>
+    </ul>
+  </li>
+  <li><a href="formfinance.php">Demande de financement</a></li>
+  <li><a href="#">Contact</a></li>
+  </ul>
+  </nav>
 <div id="slider">
 <i class="fab fa-wordpress first"></i>
 <div class="slider">

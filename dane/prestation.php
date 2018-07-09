795464cf98df0afaac6ba824b7ace3dd87d1cb97
GuillaumeDane <daneguillaume.prod@gmail.com>
2018-07-09 14:49:52 +0200

    +

---
+++ dane/prestation.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/prestation.css">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <?php
+      include ('nav.php');
+       ?>
+    </header>
+
+    <div class="prestations">
+      <h2>Courtier en crédits professionnels</h2><br>
+      <p>Recherche de financements : crédits professionnels, leasing, Crédit-Bail, financement participatif</p>
+    </div>
+
+    <div class="prestations">
+      <h2>Courtier en crédits immobiliers</h2><br>
+      <p>Recherche de financements : crédits immobiliers, crédits à la consommation</p>
+    </div>
+
+    <div class="prestations">
+      <h2>Courtier en Assurances Emprunteurs</h2><br>
+      <p>Proposition d’Assurances Emprunteurs pour les professionnels et les particuliers.</p>
+    </div>
+
+    <div class="prestations">
+      <h2>Conseil pour gérer les finances de votre entreprise</h2><br>
+      <p>Conseil en gestion financière d’entreprise : gestion de trésorerie, optimisation fiscale, choix des investissements</p>
+    </div>
+
+    <div class="prestations">
+      <h2>Formation</h2><br>
+      <p>Formation en finances d’entreprises : Business Plan, approches comptables et financières d’une entreprises</p>
+    </div>
+
+    <footer>
+      <?php
+      include ('nav.php');
+       ?>
+    </footer>
+
+  </body>
+</html>

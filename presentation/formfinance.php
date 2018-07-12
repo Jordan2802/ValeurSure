3c1246ab4c2e242b3cbcb1dda86f28b252b42472
Jordan2802 <sha08dow@hotmail.fr>
2018-07-12 17:23:29 +0200

    dfg

--- presentation/formfinance.php
+++ presentation/formfinance.php
@@ -72,10 +72,10 @@ session_start();
 
   <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script type="text/javascript" src="jquery.validate.min.js"></script> -->
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script type="text/javascript" src="js/formfinance.js"></script>
   <script type="text/javascript" src="js/burger.js"></script>
   <script type="text/javascript" src="js/contact.js"></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+
 
 </html>

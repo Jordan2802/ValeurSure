99f787408f57e0120288b0935e1173a9c39607c8
Jordan2802 <sha08dow@hotmail.fr>
2018-07-12 15:43:25 +0200

    zer

--- presentation/includes/footer.php
+++ presentation/includes/footer.php
@@ -2,38 +2,25 @@
 
 
     <div class="accueil">
-      <h3><a href="#">Accueil</a></h3>
+      <h3><a href="./index.php">Accueil</a></h3>
       <ul>
-        <li><a href="#">A propos de ValeurSur</a></li>
-        <li><a href="#">Nos prestation</a></li>
-        <li><a href="#">Simulateurs</a></li>
-        <li><a href="#">Demande de financement</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </div>
-    <div class="valeursur">
-      <h3><a href="#">À propos de ValeurSur</a></h3>
-      <ul>
-        <li><a href="#">Présentation</a></li>
-        <li><a href="#">Engagements</a></li>
-        <li><a href="#">Valeurs</a></li>
+        <li><a href="./apropos.php">A	propos de ValeurSure</a></li>
+        <li><a href="./prestation.php">Nos prestations</a></li>
+        <li><a href="./Simulateurs.php">Simulateurs</a></li>
+        <li><a href="./formfinance.php">Demande de financement</a></li>
+        <li><a href="./contact.php">Contact</a></li>
       </ul>
     </div>
     <div class="prestation">
-      <h3><a href="#">Nos Prestation</a></h3>
-      <ul>
-        <li><a href="#">Crédits professionnels</a></li>
-        <li><a href="#">Crédits immobiliers</a></li>
-        <li><a href="#">Assurances Emprunteurs</a></li>
-        <li><a href="#">Conseils pour gérer les finances de votre entreprise</a></li>
-        <li><a href="#">Formation</a></li>
+      <h3><a href="./prestation.php">Nos prestations</a></h3>
+      <<ul>
+        <li><a href="./prestation.php#lien-creditpro">Crédits professionnels</a></li>
+        <li><a href="./prestation.php#lien-creditimmo">Crédit immobiliers</a></li>
+        <li><a href="./prestation.php#lien-emprunt">Assurances Emprunteurs</a></li>
+        <li><a href="./prestation.php#lien-finances">Conseils pour gérer les finances de votre entreprise</a></li>
+        <li><a href="./prestation.php#lien-formation">Formation</a></li>
       </ul>
     </div>
-    <div class="simulateurs">
-      <h3><a href="#">Simulateurs</a></h3>
-      
-    </div>
-
    <div class="legal">
      <p><a href="#">Mentions Légales</a></p>
      <p><a href="#"><i class="fab fa-twitter"></a></i>

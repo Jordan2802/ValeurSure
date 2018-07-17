0882e3b3ec31a437b6d9d429b0072e470b20b183
Jordan2802 <sha08dow@hotmail.fr>
2018-07-17 15:26:27 +0200

    yt

--- presentation/includes/nav.php
+++ presentation/includes/nav.php
@@ -33,27 +33,27 @@
     <div id="logoNav2" class="logop2">
       <i class="fab fa-wordpress "></i>
     </div>
-    <li><a class="nav-lien" href="./index.php">Accueil</a></li>
-    <li class="menu--ss"><a class="nav-lien" href="./apropos.php">A	propos de ValeurSure</a>
+    <li><a href="./index.php">Accueil</a></li>
+    <li class="menu--ss"><a href="./apropos.php">A	propos de ValeurSure</a>
       <!--  <ul>
           <li><a class="nav-lien" href="./apropos.php#lien-presentation">Présentation</a></li>
           <li><a class="nav-lien" href="./apropos.php#lien-engagement">Engagements</a></li>
           <li><a class="nav-lien" href="./apropos.php#lien-valeur">Valeurs</a></li>
         </ul>-->
     </li>
-    <li class="menu--ss"><a class="nav-lien" href="#">Nos prestations</a>
+    <li class="menu--ss"><a href="#">Nos prestations</a>
       <ul class="hide-ss-menu">
-        <li><a class="nav-lien" href="./prestation.php#lien-creditpro">Crédits professionnels</a></li>
-        <li><a class="nav-lien" href="./prestation.php#lien-creditimmo">Crédit immobiliers</a></li>
-        <li><a class="nav-lien" href="./prestation.php#lien-emprunt">Assurances Emprunteurs</a></li>
-        <li><a class="nav-lien" href="./prestation.php#lien-finances">Conseils pour gérer les finances de votre entreprise</a></li>
-        <li><a class="nav-lien" href="./prestation.php#lien-formation">Formation</a></li>
+        <li><a href="./prestation.php#lien-creditpro">Crédits professionnels</a></li>
+        <li><a href="./prestation.php#lien-creditimmo">Crédit immobiliers</a></li>
+        <li><a href="./prestation.php#lien-emprunt">Assurances Emprunteurs</a></li>
+        <li><a href="./prestation.php#lien-finances">Conseils pour gérer les finances de votre entreprise</a></li>
+        <li><a href="./prestation.php#lien-formation">Formation</a></li>
       </ul>
     </li>
-    <li class="menu--ss"><a class="nav-lien" href="./Simulateurs.php">Simulateurs</a>
+    <li class="menu--ss"><a href="./Simulateurs.php">Simulateurs</a>
 
     </li>
-    <li><a class="nav-lien" href="./formfinance.php">Demande de financement</a></li>
-    <li><a class="nav-lien" href="./contact.php">Contact</a></li>
+    <li><a href="./formfinance.php">Demande de financement</a></li>
+    <li><a href="./contact.php">Contact</a></li>
   </ul>
 </nav>

b5eae887a21f0a631a83f4f114e19207442bde92
Jordan2802 <sha08dow@hotmail.fr>
2018-07-19 15:13:27 +0200

    Merge branch 'master' of https://github.com/Jordan2802/ValeurSure

--- presentation/js/app.js
+++ presentation/js/app.js
@@ -12,7 +12,7 @@ var a = document.getElementsByClassName("nav-lien");
 window.onscroll = function() {
 
   if (document.documentElement.scrollTop > tailleHead) {
-    tel.style.display = 'block';
+    tel.style.visibility = 'visible';
     nav.style.top = "0";
     nav.style.left = "0";
     $(nav).addClass('nav--js');
@@ -21,7 +21,7 @@ window.onscroll = function() {
       a[i].className = "backup backup:hover";
     }
   } else {
-    tel.style.display = "none";
+    tel.style.visibility = "hidden";
     nav.style.top = "5%";
     nav.style.left = "5%";
     $(nav).removeClass('nav--js');

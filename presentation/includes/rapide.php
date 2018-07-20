6bd1c2fd20b8887a7f8d266bf9fd0fa0bbc73f0b
Jordan2802 <sha08dow@hotmail.fr>
2018-07-20 16:23:41 +0200

    mise  jour contact rapide

--- presentation/includes/rapide.php
+++ presentation/includes/rapide.php
@@ -1,12 +1,28 @@
 <div class="tel" id="tel">
   <div class="fas fa-mobile-alt" onclick="slideTel()"></div>
   <div class="text cache" id="tel"><span id="valeur--coor">Coordonnées ValeurSure:</span> <br> 00-00-00-00-00, adresse@hotmail.fr <br>
-    <form class="" action="index.html" method="post">
-      <label for="">Nom</label><input type="text" name="nom" value="">
-      <label for="">Prenom</label><input type="text" name="" value="">
-      <label for="">email</label><input type="mail" name="" value="">
-      <label for="">message</label> <textarea name="name" rows="6" cols="30"></textarea>
-      <input type="button" name="button" value="Envoyer">
+    <form class=""  method="post">
+      <label for="">Nom</label><input type="text" name="nom" value="" required>
+      <label for="">Prenom</label><input type="text" name="prenom" value="" required>
+      <label for="">email</label><input type="mail" name="email" value="" required>
+      <label for="">message</label> <textarea name="message" rows="6" cols="30"></textarea>
+      <input type="submit" name="button" value="Envoyer">
     </form>
   </div>
 </div>
+<?php
+
+$message = $_POST['nom'].' '.$_POST['prenom'].' '.$_POST['message']
+if(isset($_POST['message'])){
+    $position_arobase=strpos($_POST['email'],'@');
+    if($position_arobase===false)
+        echo '<p>Votre email doit comporter un arobase.</p>';
+    else {
+        $retour=mail('sha08dow@hotmail.fr','Envoi depuis page rapide', $message, 'From : '.$_POST['email']);
+        if($retour)
+            echo '<p>Votre message a été envoyé.</p>';
+        else
+            echo '<p>Erreur.</p>';
+    }
+}
+?>

5ba7874c11e660f91412fb409ae6e25e775dd333
Jordan2802 <sha08dow@hotmail.fr>
2018-07-23 15:42:50 +0200

    ert

--- presentation/includes/rapide.php
+++ presentation/includes/rapide.php
@@ -12,17 +12,21 @@
 </div>
 <?php
 
-$message = $_POST['nom'].' '.$_POST['prenom'].' '.$_POST['message']
+
+if (isset($_POST['button'])) {
+  $message = $_POST['nom'].' '.$_POST['prenom'].' '.$_POST['message'];
 if(isset($_POST['message'])){
-    $position_arobase=strpos($_POST['email'],'@');
-    if($position_arobase===false)
-        echo '<p>Votre email doit comporter un arobase.</p>';
-    else {
-        $retour=mail('sha08dow@hotmail.fr','Envoi depuis page rapide', $message, 'From : '.$_POST['email']);
-        if($retour)
-            echo '<p>Votre message a été envoyé.</p>';
-        else
-            echo '<p>Erreur.</p>';
-    }
+      $position_arobase=strpos($_POST['email'],'@');
+      if($position_arobase===false)
+          echo '<p>Votre email doit comporter un arobase.</p>';
+      else {
+          $retour=mail('sha08dow@hotmail.fr','Envoi depuis page rapide', $message, 'From : '.$_POST['email']);
+          if($retour)
+              echo '<p>Votre message a été envoyé.</p>';
+          else
+              echo '<p>Erreur.</p>';
+      }
+  }
 }
+
 ?>

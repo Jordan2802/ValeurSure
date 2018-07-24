08ce0fd2db1ea7bafac468f3e5dc407b80b36924
GuillaumeDane <daneguillaume.prod@gmail.com>
2018-07-24 14:16:44 +0200

    +

--- presentation/index.php
+++ presentation/index.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="css/media.css">
   <link rel="stylesheet" href="css/rapide.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
   <title>ValeurSure</title>
 </head>
 

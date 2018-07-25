d9cddd2c15f0bc0632d9637181f2c66a45a656e6
wheeemyse <talla.m.seck@gmail.com>
2018-07-25 15:19:00 +0200

    footer

--- presentation/index.php
+++ presentation/index.php
@@ -135,7 +135,7 @@ include('includes/rapide.php');
 
     </main>
     <footer>
-      <?php include('includes/footer.php'); ?>
+      <?php include('includes/footerv2.php'); ?>
     </footer>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script type="text/javascript" src="js/app.js"></script>

8c10b156eefc54df1e75a037e7887f754d035e04
Valloa <guillaume.kusberg@gmail.com>
2018-07-30 15:41:34 +0200

    changement anim index

--- presentation/index.php
+++ presentation/index.php
@@ -70,61 +70,72 @@ include('includes/rapide.php');
       </div>
 
       <div id="categories">
-        <section class="wow fadeInRight sec-conseil">
-          <h2 class="section-name">Conseil</h2>
-          <p class="description show--hover">Conseil en gestion financière d’entreprise : gestion de trésorerie, optimisation fiscale, choix des investissements</p>
-        </section>
 
-        <section class="wow fadeInLeft sec-cred-pro">
-          <h2 class="section-name">Courtier en crédits professionnels</h2>
-          <p class="description show--hover">Recherche de financements : crédits professionnels, leasing, Crédit-Bail, financement participatif</p>
-        </section>
+        <a href="#" class="sec-conseil">
+          <section class="wow fadeInRight">
+            <h2 class="section-name">Conseil</h2>
+            <p class="description show--hover">Conseil en gestion financière d’entreprise : gestion de trésorerie, optimisation fiscale, choix des investissements</p>
+          </section>
+        </a>
 
+        <a href="#" class="sec-cred-pro">
+          <section class="wow fadeInLeft">
+            <h2 class="section-name">Courtier en crédits professionnels</h2>
+            <p class="description show--hover">Recherche de financements : crédits professionnels, leasing, Crédit-Bail, financement participatif</p>
+          </section>
+        </a>
 
-        <section class="wow fadeInLeft sec-simul">
-          <h2 class="section-name">Simulateurs</h2>
-          <p class="description show--hover">Quod et etiam amicitia quidem saepe ad id se quidem utilitates profecta coluntur simulatione quod an coniungendam an profecta amicitia.</p>
-          <!--
-        <div class="selecteur">
-        <select name="" id="">
-        <option value="">Selectionnez un simulateur</option>
-        <option value="cap-emprunt">Capacité d'emprunt</option>
-        <option value="capital">Capital</option>
-        <option value="mensualite">Mensualité</option>
-      </select>
-      <input class="button-simul" type="button" value="Aller au simulateur">
-    </div>
-  -->
-        </section>
+        <a href="#" class="sec-simul">
+          <section class="wow fadeInLeft ">
+            <h2 class="section-name">Simulateurs</h2>
+            <p class="description show--hover">Vous souhaitez connaître vos mensualités, le montant que vous pouvez emprunter et sur quelle durée ? Obtenez ces informations en quelques clics.</p>
+            <!--
+          <div class="selecteur">
+          <select name="" id="">
+          <option value="">Selectionnez un simulateur</option>
+          <option value="cap-emprunt">Capacité d'emprunt</option>
+          <option value="capital">Capital</option>
+          <option value="mensualite">Mensualité</option>
+        </select>
+        <input class="button-simul" type="button" value="Aller au simulateur">
+      </div>
+    -->
+          </section>
+        </a>
 
-        <section class="wow fadeInUp sec-assu-empr">
-          <h2 class="section-name">Courtier en Assurances Emprunteurs</h2>
-          <p class="description show--hover">Quod et etiam amicitia quidem saepe ad id se quidem utilitates profecta coluntur simulatione quod an coniungendam an profecta amicitia. Proposition d’Assurances Emprunteurs pour les professionnels et les particuliers.</p>
-        </section>
+        <a href="#" class="sec-assu-empr">
+          <section class="wow fadeInUp ">
+            <h2 class="section-name">Courtier en Assurances Emprunteurs</h2>
+            <p class="description show--hover">Quod et etiam amicitia quidem saepe ad id se quidem utilitates profecta coluntur simulatione quod an coniungendam an profecta amicitia. Proposition d’Assurances Emprunteurs pour les professionnels et les particuliers.</p>
+          </section>
+        </a>
 
         <section class="wow fadeInRight img-2">
         </section>
 
-        <section class="wow fadeInRight sec-cred-immo">
-          <h2 class="section-name">Courtier en crédits immobiliers</h2>
-          <p class="description show--hover">Recherche de financements : crédits immobiliers, crédits à la consommation</p>
-        </section>
+        <a href="#" class="sec-cred-immo">
+          <section class="wow fadeInRight ">
+            <h2 class="section-name">Courtier en crédits immobiliers</h2>
+            <p class="description show--hover">Recherche de financements : crédits immobiliers, crédits à la consommation</p>
+          </section>
+        </a>
 
-        <section class="wow fadeInRight sec-formation">
-          <h2 class="section-name">Formation</h2>
-          <p class="description show--hover">Formation en finances d’entreprises : Business Plan, approches comptables et financières d’une entreprises</p>
-        </section>
+        <a href="#" class="sec-formation">
+          <section class="wow fadeInRight ">
+            <h2 class="section-name">Formation</h2>
+            <p class="description show--hover">Formation en finances d’entreprises : Business Plan, approches comptables et financières d’une entreprises</p>
+          </section>
+        </a>
 
-        <section class="wow fadeInLeft media-twi">
-          <a class="twitter-timeline" data-height="5000" href="https://twitter.com/_Valloa_?ref_src=twsrc%5Etfw">Tweets by _Valloa_</a>
-          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </section>
+          <section class="wow fadeInLeft media-twi">
+            <a class="twitter-timeline" data-height="5000" href="https://twitter.com/_Valloa_?ref_src=twsrc%5Etfw">Tweets by _Valloa_</a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </section>
 
-
-        <section class="wow fadeInUp media-lin">
-          <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
-          <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="fr_FR" data-type="vertical" data-theme="dark" data-vanity="guillaume-kusberg-046926166"><a class="LI-simple-link" href='https://fr.linkedin.com/in/guillaume-kusberg-046926166?trk=profile-badge'>Guillaume kusberg</a></div>
-        </section>
+          <section class="wow fadeInUp media-lin">
+            <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="fr_FR" data-type="vertical" data-theme="dark" data-vanity="guillaume-kusberg-046926166"><a class="LI-simple-link" href='https://fr.linkedin.com/in/guillaume-kusberg-046926166?trk=profile-badge'>Guillaume kusberg</a></div>
+          </section>
 
         <section class="wow fadeInUp img-1">
         </section>
